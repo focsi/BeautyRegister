216cb85d183b175b419baf8c9527a464f12c5348
focsi <afodor@geometria.hu>
2015-02-01 12:51:11 +0100

    GuI test. Read phone number

--- BeautyRegister/MainWindow.xaml.cs
+++ BeautyRegister/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using LocalDAL;
 using System.Windows;
 using System.Linq;
+using BeautyRegister.DataClasses;
 
 namespace BeautyRegister
 {
@@ -23,7 +24,18 @@ namespace BeautyRegister
 
         private void list_SelectionChanged( object sender, System.Windows.Controls.SelectionChangedEventArgs e )
         {
+            if ( e.AddedItems.Count > 0 )
+            {
+                var name = e.AddedItems[0] as string;
+                var cust = (from customer in LocalDC.Instance.GetCustomers()
+                           where customer.Name == name
+                           select customer).FirstOrDefault();
 
+    
+                if (cust != null)
+                    nameTB.Text = cust.Phone;
+            }
+            
         }
     }
 }
